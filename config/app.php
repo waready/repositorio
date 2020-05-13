171348c15f3e64858120503daa7aec1f21da63cf
waready <elbarto119@hotmail.com>
2020-05-12 20:07:06 -0500

    merge

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        //Codedge\Fpdf\FpdfServiceProvider::class,
+        Codedge\Fpdf\FpdfServiceProvider::class,
 
     ],
 
@@ -226,7 +226,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        //'Fpdf' => Codedge\Fpdf\Facades\Fpdf::class,
+        'Fpdf' => Codedge\Fpdf\Facades\Fpdf::class,
 
     ],
 
